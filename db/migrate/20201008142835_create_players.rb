e9465912399876bdee9d23a01164c3d474d45948
github username <github email address>
2020-10-08 09:17:55 -0600

    fixes db and seed file

--- db/migrate/20201008142835_create_players.rb
+++ db/migrate/20201008142835_create_players.rb
@@ -2,6 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
   def change
     create_table :players do |t|
       t.string :username
+      t.integer :game_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_144939) do
 
   create_table "players", force: :cascade do |t|
     t.string "username"
+    t.integer "game_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
