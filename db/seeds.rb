691c6a7c6dc2736ecb67c2348f09160b83c5bbde
github username <github email address>
2020-10-08 08:43:58 -0600

    player model, migration, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rachel = Player.create(username: "Rachel")
+kevin = Player.create(username: "kpad")
+nolando = Player.create(username: "Nolando")
+lilliana = Player.create(username: "Lilliana")
