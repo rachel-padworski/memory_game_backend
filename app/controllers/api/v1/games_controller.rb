c2efed15b66678129da8028d5f857fb79993e728
github username <github email address>
2020-10-19 14:33:14 -0600

    updates routes

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -6,15 +6,15 @@ class Api::V1::GamesController < ApplicationController
         # render json: games, status: 200
     end
 
-    def create
-        game = Game.new(game_params)
-        if game.save
-            render json: GameSerializer.new(game), status: accepted
-            # render json: game, status: 200
-        else
-            render json: {errors: game.errors.full_messages}, status: :unprocessible_entity
-        end
-    end
+    # def create
+    #     game = Game.new(game_params)
+    #     if game.save
+    #         render json: GameSerializer.new(game), status: accepted
+    #         # render json: game, status: 200
+    #     else
+    #         render json: {errors: game.errors.full_messages}, status: :unprocessible_entity
+    #     end
+    # end
 
     private
 
