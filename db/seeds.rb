ff7555e93c1d59d8a3b1dba2c7809d1f67ea3399
github username <github email address>
2020-10-21 15:29:54 -0600

    adds src

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,70 +14,70 @@ players = Player.create([{username: "rachel", game_id: 1}, {username: "kpad", ga
 cards = Card.create([
          {
             title: 'felix',
-            image_url: 'memory_game_frontend/images/Felix100x100.png',
+            image_url: '../src/images/Felix100x100.png',
             game_id: 1
          },
          {
             title: 'gene',
-            image_url: 'memory_game_frontend/images/Gene100x100.png',
+            image_url: '../src/images/Gene100x100.png',
             game_id: 1
          },
          {
             title: 'king-candy',
-            image_url: 'memory_game_frontend/images/KingCandy100x100.png',
+            image_url: '../src/images/KingCandy100x100.png',
             game_id: 1
          },
          {
             title: 'qbert',
-            image_url: 'memory_game_frontend/images/Qbert100x100.png',
+            image_url: '../src/images/Qbert100x100.png',
             game_id: 1
          },
          {
              title: 'ralph',
-             image_url: 'memory_game_frontend/images/Ralph100x100.png',
+             image_url: '../src/images/Ralph100x100.png',
              game_id: 1
          },
          {
              title: 'rancis-fluggerbutter',
-             image_url: 'memory_game_frontend/images/RancisFluggerbutter100x100.png',
+             image_url: '../src/images/RancisFluggerbutter100x100.png',
              game_id: 1
          },
          {
              title: 'sargeant-calhoun',
-             image_url: 'memory_game_frontend/images/SargeantCalhoun100x100.png',
+             image_url: '../src/images/SargeantCalhoun100x100.png',
              game_id: 1
          },
          {
              title: 'sour-bill',
-             image_url: 'memory_game_frontend/images/SourBill100x100.png',
+             image_url: '../src/images/SourBill100x100.png',
              game_id: 1
          },
          {
              title: 'taffyta-muttonfudge',
-             image_url: 'memory_game_frontend/images/TaffytaMuttonfudge100x100.png',
+             image_url: '../src/images/TaffytaMuttonfudge100x100.png',
              game_id: 1
          },
          {
              title: 'vanellope-on-ralph',
-             image_url: 'memory_game_frontend/images/VanellopeOnRalph100x100.png',
+             image_url: '../src/images/VanellopeOnRalph100x100.png',
              game_id: 1
          },
          {
              title: 'vanellope-von-schweetz',
-             image_url: 'memory_game_frontend/images/VanellopeVonSchweetz100x100.png',
+             image_url: '../src/images/VanellopeVonSchweetz100x100.png',
              game_id: 1
          },
          {
              title: 'wynchal-and-duncan',
-             image_url: 'memory_game_frontend/images/WynchalAndDuncan100x100.png',
+             image_url: '../src/images/WynchalAndDuncan100x100.png',
              game_id: 1
          },
      ])
-# Game.create(title: "Memory Game", image_url: "https:memory_game_frontend/images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", player: players.first)
+# Game.create(title: "Memory Game", image_url: "https:../src/images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", player: players.first)
 
 # rachel = Player.create(usertitle: "Rachel")
 # kevin = Player.create(usertitle: "kpad")
 # nolando = Player.create(usertitle: "Nolando")
 # lilliana = Player.create(usertitle: "Lilliana")
 
-# Game.create(title: "Memory Game", image_url: "https:memory_game_frontend/images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
+# Game.create(title: "Memory Game", image_url: "https:../src/images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")
