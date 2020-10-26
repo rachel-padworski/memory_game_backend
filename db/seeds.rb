c24738726c0efa19c39a74c6af9a20d21cc072f7
github username <github email address>
2020-10-26 10:28:41 -0600

    fixes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,66 +12,126 @@
 game = Game.create([{title: "Memory Game"}])
 players = Player.create([{username: "sunset1", password: "passw0rd"}, {username: "sunrise1", password: "passw0rd"}])
 cards = Card.create([
-         {
-            title: 'felix',
-            image_url: '../src/images/Felix100x100.png',
+        {
+        title: 'felix',
+        image_url: 'https://i.ibb.co/PZLFzbm/Felix100x100.png',
             game_id: 1
          },
          {
             title: 'gene',
-            image_url: '../src/images/Gene100x100.png',
+            image_url: 'https://i.ibb.co/sH5RyTN/Gene100x100.png',
             game_id: 1
          },
          {
             title: 'king-candy',
-            image_url: '../src/images/KingCandy100x100.png',
+            image_url: 'https://i.ibb.co/Lhdtpq8/King-Candy100x100.png',
             game_id: 1
          },
          {
             title: 'qbert',
-            image_url: '../src/images/Qbert100x100.png',
+            image_url: 'https://i.ibb.co/zRPmRHb/Qbert100x100.png',
             game_id: 1
          },
          {
              title: 'ralph',
-             image_url: '../src/images/Ralph100x100.png',
+             image_url: 'https://i.ibb.co/fX9k3LW/Ralph100x100.png',
              game_id: 1
          },
          {
              title: 'rancis-fluggerbutter',
-             image_url: '../src/images/RancisFluggerbutter100x100.png',
+             image_url: 'https://i.ibb.co/XXmMZqw/Rancis-Fluggerbutter100x100.png',
              game_id: 1
          },
          {
              title: 'sargeant-calhoun',
-             image_url: '../src/images/SargeantCalhoun100x100.png',
+             image_url: 'https://i.ibb.co/BnCh61F/Sargeant-Calhoun100x100.png',
              game_id: 1
          },
          {
              title: 'sour-bill',
-             image_url: '../src/images/SourBill100x100.png',
+             image_url: 'https://i.ibb.co/37YJ11Q/Sour-Bill100x100.png',
              game_id: 1
          },
          {
              title: 'taffyta-muttonfudge',
-             image_url: '../src/images/TaffytaMuttonfudge100x100.png',
+             image_url: 'https://i.ibb.co/6mhg9Zw/Taffyta-Muttonfudge100x100.png',
              game_id: 1
          },
          {
              title: 'vanellope-on-ralph',
-             image_url: '../src/images/VanellopeOnRalph100x100.png',
+             image_url: 'https://i.ibb.co/n69zVS9/Vanellope-On-Ralph100x100.png',
              game_id: 1
          },
          {
              title: 'vanellope-von-schweetz',
-             image_url: '../src/images/VanellopeVonSchweetz100x100.png',
+             image_url: 'https://i.ibb.co/X266dbL/Vanellope-Von-Schweetz100x100.png',
              game_id: 1
          },
          {
              title: 'wynchal-and-duncan',
-             image_url: '../src/images/WynchalAndDuncan100x100.png',
+             image_url: 'https://i.ibb.co/sPfKvb1/Wynchal-And-Duncan100x100.png',
              game_id: 1
          },
+         {
+        title: 'felix',
+        image_url: 'https://i.ibb.co/PZLFzbm/Felix100x100.png',
+            game_id: 1
+         },
+         {
+            title: 'gene',
+            image_url: 'https://i.ibb.co/sH5RyTN/Gene100x100.png',
+            game_id: 1
+         },
+         {
+            title: 'king-candy',
+            image_url: 'https://i.ibb.co/Lhdtpq8/King-Candy100x100.png',
+            game_id: 1
+         },
+         {
+            title: 'qbert',
+            image_url: 'https://i.ibb.co/zRPmRHb/Qbert100x100.png',
+            game_id: 1
+         },
+         {
+             title: 'ralph',
+             image_url: 'https://i.ibb.co/fX9k3LW/Ralph100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'rancis-fluggerbutter',
+             image_url: 'https://i.ibb.co/XXmMZqw/Rancis-Fluggerbutter100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'sargeant-calhoun',
+             image_url: 'https://i.ibb.co/BnCh61F/Sargeant-Calhoun100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'sour-bill',
+             image_url: 'https://i.ibb.co/37YJ11Q/Sour-Bill100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'taffyta-muttonfudge',
+             image_url: 'https://i.ibb.co/6mhg9Zw/Taffyta-Muttonfudge100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'vanellope-on-ralph',
+             image_url: 'https://i.ibb.co/n69zVS9/Vanellope-On-Ralph100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'vanellope-von-schweetz',
+             image_url: 'https://i.ibb.co/X266dbL/Vanellope-Von-Schweetz100x100.png',
+             game_id: 1
+         },
+         {
+             title: 'wynchal-and-duncan',
+             image_url: 'https://i.ibb.co/sPfKvb1/Wynchal-And-Duncan100x100.png',
+             game_id: 1
+         }
      ])
 # Game.create(title: "Memory Game", image_url: "https:../src/images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", player: players.first)
 
